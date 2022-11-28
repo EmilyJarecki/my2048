2c6a9015291d914cf0a6f924338307755672117b
EmilyJarecki <jareckiemily@yahoo.com>
2022-11-27 18:41:19 -0600

    create rows using modulo

--- main.js
+++ main.js
@@ -23,3 +23,17 @@ function getRandom() {
         getRandom()
     }
 }
+
+moveRight()
+function moveRight(){
+    for (let i = 0; i < width * width; i++){
+        if (i % 4 === 0) {
+            let row1 = tiles[i].innerHTML      //i= tiles[0] tiles[4] tiles [8] tiles[12]     (the start of the row and column 1)
+            let row2 = tiles[i+1].innerHTML// i+1 = tiles[1] tiles[5] tiles [9] tiles[13]
+            let row3 = tiles[i+2].innerHTML// i+2 = tiles[2] tiles[6] tiles [10] tiles[14]
+            let row4 = tiles[i+3].innerHTML// i+3 = tiles[3] tiles[7] tiles [11] tiles[15]
+            let rows = [Number(row1), Number(row2), Number(row3), Number(row4)]
+            console.log(rows)
+        }
+    }
+}
