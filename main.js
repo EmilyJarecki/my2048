18bccfaea286864ccc27284321567d51427f9240
EmilyJarecki <jareckiemily@yahoo.com>
2022-12-04 11:35:26 -0600

    adds 2 to get 4

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const width = 4
 const gameBoard = document.getElementById("game_board")
 
 window.addEventListener('keydown', (event)=>{
-    getRandom()
+    // getRandom()  
+    // <-this will add a ranodm number every move but commented out for my sanity
     if (event.key === "ArrowLeft"){
         moveLeft()
     } else if (event.key === "ArrowRight"){
@@ -26,7 +27,6 @@ function createGrid (){
         gameBoard.appendChild(square)
         tiles.push(square)
     }
-    getRandom()
     getRandom()
     getRandom()
 }
@@ -55,6 +55,7 @@ function moveRight(){
                 Number(row4)]
 
             const filtered = rows.filter(num => num !== 0)
+
             const rowZeros = rows.filter(num => num === 0)
             const concattedRow = new Array(...rowZeros, ...filtered)
 
@@ -71,7 +72,9 @@ function moveLeft(){
     for (let i = 0; i < width * width; i++){
         if (i % 4 === 0) {
             let row1 = tiles[i].innerHTML      //i= tiles[0] tiles[4] tiles [8] tiles[12]     (the start of the row and column 1)
+            console.log(row1[0])
             let row2 = tiles[i+1].innerHTML// i+1 = tiles[1] tiles[5] tiles [9] tiles[13]
+            // console.log(row2[0])
             let row3 = tiles[i+2].innerHTML// i+2 = tiles[2] tiles[6] tiles [10] tiles[14]
             let row4 = tiles[i+3].innerHTML// i+3 = tiles[3] tiles[7] tiles [11] tiles[15]
             let rows = [
@@ -79,7 +82,6 @@ function moveLeft(){
                 Number(row2), 
                 Number(row3), 
                 Number(row4)]
-            
             const filtered = rows.filter(num => num !== 0)
             const rowZeros = rows.filter(num => num === 0)
             const concattedRow = new Array(...filtered, ...rowZeros)            
@@ -88,9 +90,17 @@ function moveLeft(){
             tiles[i+1].innerHTML = concattedRow[1]
             tiles[i+2].innerHTML = concattedRow[2]
             tiles[i+3].innerHTML = concattedRow[3]
+            console.log(concattedRow[0])
+
+            if (tiles[i].innerHTML===tiles[i+1].innerHTML){
+                let newTiles = tiles[i].innerHTML*=2
+                tiles[i].innerHTML = newTiles
+                tiles[i+1].innerHTML= 0
+            }
         }
     }
 }
+
 
 function moveUp(){
     for (let i = 0; i < width; i++){
